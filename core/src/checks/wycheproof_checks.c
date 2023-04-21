8050ac83bc09944f66f6f7fc91f0aa8c09e343b4
Sean Andersen <seana@squareup.com>
2023-04-21 13:20:19 -0400

    wip

--- core/src/checks/wycheproof_checks.c
+++ core/src/checks/wycheproof_checks.c
@@ -4,6 +4,31 @@
 #include "log.h"
 
 int verify_wycheproof(void) {
+  #include "wycheproof/ecdsa_secp256k1_sha256_bitcoin_test.h"
   INFO("foobar 373737!");
+
+  int t;
+  for (t = 0; t < SECP256K1_ECDSA_WYCHEPROOF_NUMBER_TESTVECTORS; t++) {
+    INFO("***");
+    const unsigned char *msg, *sig, *pk;
+    (void)msg;
+    (void)sig;
+    (void)pk;
+
+    // trezor pubkey...
+    const uint8_t *pubkey;
+    memset(&pubkey, 0, sizeof(pubkey));
+
+    //   const ecdsa_curve *curve = &secp256k1;
+    // int ecdsa_read_pubkey(const ecdsa_curve *curve, const uint8_t *pub_key, curve_point *pub) {
+
+    pk = &wycheproof_ecdsa_public_keys[testvectors[t].pk_offset];
+
+    msg = &wycheproof_ecdsa_messages[testvectors[t].msg_offset];
+
+    sig = &wycheproof_ecdsa_signatures[testvectors[t].sig_offset];
+
+  }
+
   return 0;
 }
